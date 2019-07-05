d253575cfe960b6028e476c65c3f909f24707a98
Bram Wiepjes <bramw@protonmail.com>
2019-07-05 11:48:39 +0200

    created backend tests

--- backend/src/baserow/api/v0/user/urls.py
+++ backend/src/baserow/api/v0/user/urls.py
@@ -13,5 +13,5 @@ urlpatterns = [
     url(r'^token-auth/$', obtain_jwt_token, name='token_auth'),
     url(r'^token-refresh/$', refresh_jwt_token, name='token_refresh'),
     url(r'^token-verify/$', verify_jwt_token, name='token_verify'),
-    url(r'^$', UserView.as_view(), name='user')
+    url(r'^$', UserView.as_view(), name='index')
 ]

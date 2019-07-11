69585159a6e913738eaa2dbae16dfe20a422e738
Bram Wiepjes <bramw@protonmail.com>
2019-07-11 19:04:19 +0200

    added postgres to backend pytest gitlab ci

--- backend/src/baserow/config/settings/test.py
+++ backend/src/baserow/config/settings/test.py
@@ -1,1 +1,13 @@
 from .base import *  # noqa: F403, F401
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'baserow',
+        'USER': 'baserow',
+        'PASSWORD': 'baserow',
+        'HOST': 'db',
+        'PORT': '5432',
+    }
+}

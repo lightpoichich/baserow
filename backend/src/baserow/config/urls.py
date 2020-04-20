e95c8afc4803f734c5e5eef4e90f8557fd3dc63c
Bram Wiepjes <bramw@protonmail.com>
2020-04-20 19:16:03 +0200

    implemented health check pages

--- backend/src/baserow/config/urls.py
+++ backend/src/baserow/config/urls.py
@@ -1,7 +1,13 @@
 from django.urls import include
 from django.conf.urls import url
+from django.http import HttpResponse
+
+
+def health(request):
+    return HttpResponse('OK')
 
 
 urlpatterns = [
     url(r'^api/v0/', include('baserow.api.v0.urls', namespace='api_v0')),
+    url(r'^_health', health , name='health_check'),
 ]

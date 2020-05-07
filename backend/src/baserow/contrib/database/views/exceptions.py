fd26dec7442ad85d53e81fab8e1b702e2e5cfb2f
Bram Wiepjes <bramw@protonmail.com>
2020-05-07 22:59:46 +0200

    optionally include field_options in the grid view listing response and added an endpoint to update the field options

--- backend/src/baserow/contrib/database/views/exceptions.py
+++ backend/src/baserow/contrib/database/views/exceptions.py
@@ -7,6 +7,13 @@ class ViewDoesNotExist(Exception):
     """Raised when trying to get a view that doesn't exist."""
 
 
+class UnrelatedFieldError(Exception):
+    """
+    Raised when a field is not related to the view. For example when unrelated field
+    options are being updated.
+    """
+
+
 class ViewTypeAlreadyRegistered(InstanceTypeAlreadyRegistered):
     pass
 

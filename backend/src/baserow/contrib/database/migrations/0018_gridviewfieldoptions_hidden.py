f16ef920b5443a17640cb9f64513ac1060b00d67
Keith Axline <keith@axline.io>
2020-11-27 16:29:29 -0800

    Fixing migration

--- backend/src/baserow/contrib/database/migrations/0018_gridviewfieldoptions_hidden.py
+++ backend/src/baserow/contrib/database/migrations/0018_gridviewfieldoptions_hidden.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('database', '0001_initial'),
+        ('database', '0017_view_filters_disabled'),
     ]
 
     operations = [

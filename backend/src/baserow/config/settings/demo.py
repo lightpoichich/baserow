6f66855dc9f362cb73d1b4d4b558336e3497322c
Jannis Gebauer <ja.geb@me.com>
2021-03-14 11:49:40 +0100

    sent all emails via a Celery task instead of synchronous

--- backend/src/baserow/config/settings/demo.py
+++ backend/src/baserow/config/settings/demo.py
@@ -2,4 +2,4 @@ from .base import *  # noqa: F403, F401
 
 
 DEBUG = True
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+CELERY_EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'

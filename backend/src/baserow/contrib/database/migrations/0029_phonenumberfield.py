f48b3431ec433203bc33c204d5302abb8d3fcffb
Nigel Gott <nigel.gott@gmail.com>
2021-03-16 13:34:53 +0000

    Fix migration numbering conflict.

--- backend/src/baserow/contrib/database/migrations/0029_phonenumberfield.py
+++ backend/src/baserow/contrib/database/migrations/0029_phonenumberfield.py
@@ -5,7 +5,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ('database', '0027_gridviewfieldoptions_order'),
+        ('database', '0028_fix_negative_date'),
     ]
 
     operations = [

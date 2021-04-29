2ce496427791408f9e7c79fbac2bb5ee6319977c
Nigel Gott <nigel@baserow.io>
2021-04-29 12:41:20 +0100

    Add fullstops to error message.

--- web-frontend/modules/core/plugins/clientHandler.js
+++ web-frontend/modules/core/plugins/clientHandler.js
@@ -63,11 +63,11 @@ class ErrorHandler {
       ),
       USER_ADMIN_CANNOT_DEACTIVATE_SELF: new ResponseErrorMessage(
         'Action not allowed.',
-        'You cannot de-activate or un-staff yourself'
+        'You cannot de-activate or un-staff yourself.'
       ),
       USER_ADMIN_CANNOT_DELETE_SELF: new ResponseErrorMessage(
         'Action not allowed.',
-        'You cannot delete yourself'
+        'You cannot delete yourself.'
       ),
     }
 

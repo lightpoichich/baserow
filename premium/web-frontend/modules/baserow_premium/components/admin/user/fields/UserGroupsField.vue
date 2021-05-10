2d8e087ca086e450db3859fba9a859287fa88f5c
Nigel Gott <nigel@baserow.io>
2021-05-10 13:24:45 +0100

    Rely on the DOM/CSS to calculate if the expand button will overflow or not and update its order accordingly. Add more comments.

--- premium/web-frontend/modules/baserow_premium/components/admin/user/fields/UserGroupsField.vue
+++ premium/web-frontend/modules/baserow_premium/components/admin/user/fields/UserGroupsField.vue
@@ -20,7 +20,7 @@
       </span>
       <a
         v-show="overflowing"
-        ref="expandButton"
+        ref="expand"
         class="user-admin-group__expand"
         :style="{
           order: expandOrder,
@@ -90,69 +90,77 @@ export default {
     /**
      * Calculates how many groups fit into the groups cell, if any are overflowing and
      * do not fit we add a + button to display a context menu showing these hidden
-     * groups.
+     * groups. We rely on flexbox wrapping to make groups which do not fit in the cell
+     * wrap down to a new row below which is invisible.
      */
     recalculateHiddenGroups() {
       if (process.server) {
         return
       }
+
       this.$nextTick(() => {
-        let numHiddenGroups = this.groups.length
-        // The starting empty element never flex-wraps down into a new row. So if
-        // a group after it has the same top value then it must not have wrapped down
-        // and hence must fit and be visible.
-        const emptyElementTop = this.$refs.empty.getBoundingClientRect().top
-        // We calculate how much space is left on the first visible row as we go
-        let firstRowSpaceRemaining = this.$refs.groupsContainer.clientWidth
-        // Then we dynamically change the order of the expand button to slot it into
-        // the correct place depending on if groups are hidden or not.
         let expandOrder = 0
+        let numHiddenGroups = this.groups.length
+
+        // The starting empty element never flex-wraps down into a new row as it has 0
+        // width.
+        // If a element after it has the same top value then it must not have wrapped
+        // down and hence must fit and be visible. If the elements top value is greater
+        // then it must have overflowed and wrapped into a row below, causing it to
+        // become invisible.
+        // Comparing top values is done instead of trying to
+        // manually calculate which groups fit using el.client/scrollWidth because:
+        // 1. We don't want to worry about exactly calculating the widths of each group
+        //    as there are possibly bugs with this in Vue (scrollWidth etc can return
+        //    invalid numbers in some unknown situations).
+        // 2. By using the tops we leave it entirely up to CSS and the DOM to figure out
+        //    what fits or not reducing the complexity of this method. We don't want
+        //    to try and replicate the overflow calculations ourselves as then we need
+        //    to maintain code that matches the browser calculation in every possible
+        //    situation.
+        const emptyElementTop = this.$refs.empty.getBoundingClientRect().top
+
         for (let i = 0; i < this.$refs.groups.length; i++) {
           const groupEl = this.$refs.groups[i]
           const groupTop = groupEl.getBoundingClientRect().top
           if (groupTop > emptyElementTop) {
-            // A groupEl element has been flex-wrapped down into a new row due to no
+            // This groupEl element has been flex-wrapped down into a new row due to no
             // space. Every group after this one must also be hidden hence we now have
-            // calculated the number of hidden groups.
-            const expandButtonWidth = this.$refs.expandButton.scrollWidth
+            // calculated the number of hidden groups and can break.
 
-            const wrappingErrorMargin = 15
-            if (
-              firstRowSpaceRemaining - groupEl.scrollWidth >
-                wrappingErrorMargin &&
-              numHiddenGroups === 1
-            ) {
-              // However first check, if this group which has been wrapped down would
-              // fit on the first row (with some room for error) if the expand button
-              // was not also on the first row. If that is so AND there is only one
-              // hidden group we can safely hide the expand button and let the
-              // group wrap back into fitting on the first row.
-              expandOrder = i
-              numHiddenGroups--
-            } else if (
-              firstRowSpaceRemaining >
-              expandButtonWidth + wrappingErrorMargin
-            ) {
-              // We need to show the expand button and there is room for it on the first
-              // row so we can put it after the last visible group on the first row.
-              expandOrder = i - 1
-            } else {
-              // There is no room on the first row for the expand button, so we put it
-              // before the last group on the first row which will cause that group
-              // to wrap down and no longer be visible.
-              expandOrder = i - 2
-              numHiddenGroups++
-            }
+            // Insert the expand button before this hidden group. This button might also
+            // wrap down and become invisible hence we later on recheck if this has
+            // happened or not.
+            expandOrder = i - 1
             break
           } else {
+            // Insert the expand button after the latest visible group
             expandOrder = i
-            firstRowSpaceRemaining -= groupEl.scrollWidth
             numHiddenGroups--
           }
         }
         this.expandOrder = expandOrder
         this.numHiddenGroups = numHiddenGroups
         this.overflowing = numHiddenGroups !== 0
+
+        // Check that the expand button hasn't wrapped down and become invisible because
+        // there wasn't enough room to fit it on the first row. If so move it back one
+        // further in the order so it replaces the last visible group in the row instead
+        if (this.overflowing) {
+          // We have to wait a tick for the DOM to recalculate to see if the inserted
+          // expand button fits or not. This is much easier, simpler and cleaner
+          // than attempting to do the calculation CSS/DOM will do for us ourselves!
+          this.$nextTick(() => {
+            const emptyElementTop = this.$refs.empty.getBoundingClientRect().top
+            const expandTop = this.$refs.expand.getBoundingClientRect().top
+            if (expandTop > emptyElementTop) {
+              this.expandOrder--
+              // By moving the button back one we will make the last group overflow,
+              // wrap and become invisible so we update numHiddenGroups accordingly.
+              this.numHiddenGroups++
+            }
+          })
+        }
       })
     },
   },

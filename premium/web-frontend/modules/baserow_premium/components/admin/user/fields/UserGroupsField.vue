d4adb9ab7c9404321f1ef3263859ee6b2a52881f
Nigel Gott <nigel@baserow.io>
2021-05-10 11:18:04 +0100

    Use flexbox dynamic ordering to ensure that the hidden group expand button is not right aligned but always next to the last group.

--- premium/web-frontend/modules/baserow_premium/components/admin/user/fields/UserGroupsField.vue
+++ premium/web-frontend/modules/baserow_premium/components/admin/user/fields/UserGroupsField.vue
@@ -1,12 +1,15 @@
 <template>
   <div class="user-admin-group" v-on="$listeners">
-    <div class="user-admin-group__container">
+    <div ref="groupsContainer" class="user-admin-group__container">
       <span ref="empty" class="user-admin-group__empty-item"></span>
       <span
-        v-for="group in groups"
+        v-for="(group, index) in groups"
         ref="groups"
         :key="'admin-row-group' + userId + '-' + group.id"
         class="user-admin-group__item"
+        :style="{
+          order: index,
+        }"
       >
         {{ group.name }}
         <i
@@ -15,13 +18,17 @@
           class="user-admin-group__icon fas fa-users-cog"
         ></i>
       </span>
+      <a
+        v-show="overflowing"
+        ref="expandButton"
+        class="user-admin-group__expand"
+        :style="{
+          order: expandOrder,
+        }"
+        @click.prevent="showContext"
+        >+{{ numHiddenGroups }}</a
+      >
     </div>
-    <a
-      v-show="overflowing"
-      class="user-admin-group__expand"
-      @click.prevent="showContext"
-      >+{{ numHiddenGroups }}</a
-    >
   </div>
 </template>
 <script>
@@ -48,6 +55,7 @@ export default {
       overflowing: false,
       numHiddenGroups: 0,
       renderContext: false,
+      expandOrder: -1,
     }
   },
   computed: {
@@ -94,6 +102,11 @@ export default {
         // a group after it has the same top value then it must not have wrapped down
         // and hence must fit and be visible.
         const emptyElementTop = this.$refs.empty.getBoundingClientRect().top
+        // We calculate how much space is left on the first visible row as we go
+        let firstRowSpaceRemaining = this.$refs.groupsContainer.clientWidth
+        // Then we dynamically change the order of the expand button to slot it into
+        // the correct place depending on if groups are hidden or not.
+        let expandOrder = 0
         for (let i = 0; i < this.$refs.groups.length; i++) {
           const groupEl = this.$refs.groups[i]
           const groupTop = groupEl.getBoundingClientRect().top
@@ -101,11 +114,43 @@ export default {
             // A groupEl element has been flex-wrapped down into a new row due to no
             // space. Every group after this one must also be hidden hence we now have
             // calculated the number of hidden groups.
+            const expandButtonWidth = this.$refs.expandButton.scrollWidth
+
+            const wrappingErrorMargin = 15
+            if (
+              firstRowSpaceRemaining - groupEl.scrollWidth >
+                wrappingErrorMargin &&
+              numHiddenGroups === 1
+            ) {
+              // However first check, if this group which has been wrapped down would
+              // actually fit on the first row (with some room for error) then the
+              // expand button itself is causing it to wrap down. If there is only one
+              // hidden group then we can move the expand button so it does actually
+              // fit on the first row there is then no need to show the expand button.
+              expandOrder = i
+              numHiddenGroups--
+            } else if (
+              firstRowSpaceRemaining >
+              expandButtonWidth + wrappingErrorMargin
+            ) {
+              // We need to show the expand button and there is room for it on the first
+              // row so we can put it after the last visible group on the first row.
+              expandOrder = i - 1
+            } else {
+              // There is no room on the first row for the expand button, so we put it
+              // before the last group on the first row which will cause that group
+              // to wrap down and no longer be visible.
+              expandOrder = i - 2
+              numHiddenGroups++
+            }
             break
           } else {
+            expandOrder = i
+            firstRowSpaceRemaining -= groupEl.scrollWidth
             numHiddenGroups--
           }
         }
+        this.expandOrder = expandOrder
         this.numHiddenGroups = numHiddenGroups
         this.overflowing = numHiddenGroups !== 0
       })

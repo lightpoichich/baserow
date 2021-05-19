e53d4c4332938e6d41724487d9d5ae85fc37b577
Bram Wiepjes <bramw@protonmail.com>
2021-05-19 16:41:50 +0200

    added html/css of the dashboard page

---
+++ premium/web-frontend/modules/baserow_premium/components/admin/dashboard/chars/ActiveUsers.vue
@@ -0,0 +1,104 @@
+<script>
+import { Line } from 'vue-chartjs'
+
+export default {
+  extends: Line,
+  mounted() {
+    this.renderChart(
+      {
+        labels: [
+          new Date(2021, 0, 1),
+          new Date(2021, 0, 2),
+          new Date(2021, 0, 3),
+          new Date(2021, 0, 4),
+          new Date(2021, 0, 5),
+          new Date(2021, 0, 6),
+          new Date(2021, 0, 7),
+          new Date(2021, 0, 8),
+          new Date(2021, 0, 9),
+          new Date(2021, 0, 10),
+          new Date(2021, 0, 11),
+          new Date(2021, 0, 12),
+          new Date(2021, 0, 13),
+          new Date(2021, 0, 14),
+          new Date(2021, 0, 15),
+          new Date(2021, 0, 16),
+          new Date(2021, 0, 17),
+          new Date(2021, 0, 18),
+          new Date(2021, 0, 19),
+          new Date(2021, 0, 20),
+          new Date(2021, 0, 21),
+          new Date(2021, 0, 22),
+          new Date(2021, 0, 23),
+          new Date(2021, 0, 24),
+          new Date(2021, 0, 25),
+          new Date(2021, 0, 26),
+          new Date(2021, 0, 27),
+          new Date(2021, 0, 28),
+          new Date(2021, 0, 29),
+          new Date(2021, 0, 30),
+        ],
+        datasets: [
+          {
+            label: '',
+            borderColor: '#198dd6',
+            backgroundColor: '#ebf7ff',
+            color: '#b4bac2',
+            data: [
+              40,
+              39,
+              10,
+              40,
+              39,
+              80,
+              40,
+              40,
+              39,
+              10,
+              40,
+              39,
+              80,
+              40,
+              40,
+              39,
+              10,
+              40,
+              39,
+              80,
+              40,
+              40,
+              39,
+              10,
+              40,
+              39,
+              80,
+              40,
+              10,
+              20,
+            ],
+          },
+        ],
+      },
+      {
+        responsive: true,
+        maintainAspectRatio: false,
+        legend: {
+          display: false,
+        },
+        scales: {
+          xAxes: [
+            {
+              type: 'time',
+              time: {
+                displayFormats: {
+                  quarter: 'MMM D',
+                },
+              },
+            },
+          ],
+        },
+      }
+    )
+  },
+}
+</script>
---
+++ premium/web-frontend/modules/baserow_premium/pages/admin/dashboard.vue
@@ -0,0 +1,137 @@
+<template>
+  <div class="layout__col-2-scroll">
+    <div class="admin-dashboard">
+      <h1>Dashboard</h1>
+      <div class="row margin-bottom-3">
+        <div class="col col-4">
+          <div class="admin-dashboard__box">
+            <div class="admin-dashboard__box-title">Totals</div>
+            <div class="admin-dashboard__numbers">
+              <div class="admin-dashboard__numbers-name">Total users</div>
+              <div class="admin-dashboard__numbers-value">6336</div>
+              <div class="admin-dashboard__numbers-percentage">
+                <a href="#">view all</a>
+              </div>
+            </div>
+            <div class="admin-dashboard__numbers">
+              <div class="admin-dashboard__numbers-name">Total groups</div>
+              <div class="admin-dashboard__numbers-value">8762</div>
+              <div class="admin-dashboard__numbers-percentage">
+                <a href="#">view all</a>
+              </div>
+            </div>
+            <div class="admin-dashboard__numbers">
+              <div class="admin-dashboard__numbers-name">
+                Total applications
+              </div>
+              <div class="admin-dashboard__numbers-value">6336</div>
+            </div>
+          </div>
+        </div>
+        <div class="col col-4">
+          <div class="admin-dashboard__box">
+            <div class="admin-dashboard__box-title">New users</div>
+            <div class="admin-dashboard__numbers">
+              <div class="admin-dashboard__numbers-name">
+                New users last 24 hours
+              </div>
+              <div class="admin-dashboard__numbers-value">8</div>
+              <div class="admin-dashboard__numbers-percentage">
+                <span class="admin-dashboard__numbers-percentage-value"
+                  >+ 10.23%</span
+                >
+              </div>
+            </div>
+            <div class="admin-dashboard__numbers">
+              <div class="admin-dashboard__numbers-name">
+                New users last 7 days
+              </div>
+              <div class="admin-dashboard__numbers-value">8</div>
+              <div class="admin-dashboard__numbers-percentage">
+                <span class="admin-dashboard__numbers-percentage-value"
+                  >+ 10.23%</span
+                >
+              </div>
+            </div>
+            <div class="admin-dashboard__numbers">
+              <div class="admin-dashboard__numbers-name">
+                New users last 24 hours
+              </div>
+              <div class="admin-dashboard__numbers-value">8</div>
+              <div class="admin-dashboard__numbers-percentage">
+                <span
+                  class="admin-dashboard__numbers-percentage-value admin-dashboard__numbers-percentage-value--negative"
+                  >- 10.23%</span
+                >
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col col-4">
+          <div class="admin-dashboard__box">
+            <div class="admin-dashboard__box-title">Active users</div>
+            <div class="admin-dashboard__numbers">
+              <div class="admin-dashboard__numbers-name">
+                Active users last 24 hours
+              </div>
+              <div class="admin-dashboard__numbers-value">8</div>
+              <div class="admin-dashboard__numbers-percentage">
+                <span class="admin-dashboard__numbers-percentage-value"
+                  >+ 10.23%</span
+                >
+              </div>
+            </div>
+            <div class="admin-dashboard__numbers">
+              <div class="admin-dashboard__numbers-name">
+                Active users last 7 days
+              </div>
+              <div class="admin-dashboard__numbers-value">8</div>
+              <div class="admin-dashboard__numbers-percentage">
+                <span class="admin-dashboard__numbers-percentage-value"
+                  >+ 10.23%</span
+                >
+              </div>
+            </div>
+            <div class="admin-dashboard__numbers">
+              <div class="admin-dashboard__numbers-name">
+                Active users last 24 hours
+              </div>
+              <div class="admin-dashboard__numbers-value">8</div>
+              <div class="admin-dashboard__numbers-percentage">
+                <span
+                  class="admin-dashboard__numbers-percentage-value admin-dashboard__numbers-percentage-value--negative"
+                  >- 10.23%</span
+                >
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="admin-dashboard__box">
+        <div class="admin-dashboard__box-dropdown">
+          <Dropdown :value="'active_users_per_day'" :show-search="false">
+            <DropdownItem
+              name="Active users per day"
+              value="active_users_per_day"
+            ></DropdownItem>
+            <DropdownItem
+              name="New users per day"
+              value="new_users_per_day"
+            ></DropdownItem>
+          </Dropdown>
+        </div>
+        <ActiveUsers></ActiveUsers>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import ActiveUsers from '@baserow_premium/components/admin/dashboard/chars/ActiveUsers'
+
+export default {
+  components: { ActiveUsers },
+  layout: 'app',
+  middleware: 'staff',
+}
+</script>

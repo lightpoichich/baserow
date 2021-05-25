22b840e472d307b01127841ea8b778f7be60c059
Bram Wiepjes <bramw@protonmail.com>
2021-05-25 23:49:36 +0200

    added human readable error message when the user tries to sign in with a deactivated account

--- backend/src/baserow/config/settings/base.py
+++ backend/src/baserow/config/settings/base.py
@@ -128,6 +128,12 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# We need the `AllowAllUsersModelBackend` in order to respond with a proper error
+# message when the user is not active. The only thing it does, is allowing non active
+# users to authenticate, but the user still can't authenticate because the
+# `JSONWebTokenSerializer` does not allow not active users.
+AUTHENTICATION_BACKENDS = ["django.contrib.auth.backends.AllowAllUsersModelBackend"]
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/

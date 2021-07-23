1f6321e68f826ebdd08e5869cad4ca932472ea01
Sascha Jullmann <sascha@MBP-von-Sascha.fritz.box>
2021-07-23 13:52:03 +0200

    changed behavior of printable character function

--- web-frontend/modules/core/utils/events.js
+++ web-frontend/modules/core/utils/events.js
@@ -1,64 +1,17 @@
-export function isUnicodeLetterCharacter(str) {
-  /*
-  This function determines whether the pressed key is a alphabetic character
-  if (charRegex.test(str)) {
-    return true
-  }
-  return false
-   */
-  const charRegex = /^\p{L}\p{M}*$/iu
-  return charRegex.test(str)
-}
-
-export function isUnicodeNumberCharacter(str) {
-  /*
-  This function determines whether the pressed key is a number
-   */
-  const numberRegex = /^\p{N}$/iu
-  if (numberRegex.test(str)) {
-    return true
-  }
-  return false
-}
-
-export function isUnicodeSymbolCharacter(str) {
-  /*
-  This function determines whether the pressed key is a math symbol, currency sign, etc.
-   */
-  const symbolRegex = /^\p{S}$/iu
-  if (symbolRegex.test(str)) {
-    return true
-  }
-  return false
-}
-
-export function isUnicodePunctuationCharacter(str) {
-  /*
-  This function determines whether the pressed key is a punctuation character
-   */
-  const punctuationRegex = /^\p{P}$/iu
-  if (punctuationRegex.test(str)) {
-    return true
-  }
-  return false
-}
-
 export function isPrintableUnicodeCharacterKeyPress(event) {
   /*
   This function is a helper which determines whether the pressed key
-  is either a
-    UnicodeLetterCharacter or
-    UnicodeNumberCharacter or
-    UnicodeSymbolCharacter or
-    UnicodePunctuationCharacter
-  hereby defined as a 'printable character'
+  is 'not a Control Key Character'
+  thereby determining if the key is a 'printable character'
    */
-  const { key } = event
-  const isPrintableCharacter =
-    isUnicodeLetterCharacter(key) ||
-    isUnicodeNumberCharacter(key) ||
-    isUnicodeSymbolCharacter(key) ||
-    isUnicodePunctuationCharacter(key)
 
-  return isPrintableCharacter
+  if (event == null) {
+    return false
+  }
+  const { key } = event
+  const nonControlCharacterRegex = /^\P{C}$/iu
+  if (nonControlCharacterRegex.test(key)) {
+    return true
+  }
+  return false
 }
--- web-frontend/test/unit/core/utils/events.spec.js
+++ web-frontend/test/unit/core/utils/events.spec.js
@@ -1,11 +1,6 @@
-import {
-  isUnicodeLetterCharacter,
-  isUnicodeNumberCharacter,
-  isUnicodePunctuationCharacter,
-  isUnicodeSymbolCharacter,
-} from '@baserow/modules/core/utils/events'
+import { isPrintableUnicodeCharacterKeyPress } from '@baserow/modules/core/utils/events'
 
-describe('test key press event helpers', () => {
+describe('test key press event helper', () => {
   const aToZLower = [
     'a',
     'b',
@@ -75,12 +70,12 @@ describe('test key press event helpers', () => {
     'Ź',
   ]
 
-  const notString = [{}, 5, []]
+  const notString = [{}, [], null, undefined]
 
   const aToZMoreThanOneChar = ['aa', 'Aa', 'bbb', 'BBB']
 
-  const testNumberCases = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-  const testNumberCasesNumber = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+  const numberStrings = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+  const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
   const symbolCharacters = [
     '<',
@@ -124,164 +119,85 @@ describe('test key press event helpers', () => {
     '*',
   ]
 
-  const specialCharacterCases = [...symbolCharacters, ...punctuationCharacters]
+  const separatorCharacters = [
+    ' ',
+    ' ',
+    ' ',
+    ' ',
+    ' ',
+    ' ',
+    ' ',
+    ' ',
+    ' ',
+    ' ',
+    ' ',
+    ' ',
+    ' ',
+    ' ',
+    ' ',
+    ' ',
+    '　',
+  ]
 
-  // Tests for isAlphabeticCharacter function
-  test.each(aToZLower)(
-    'test letter character lower should be true',
-    (value) => {
-      expect(isUnicodeLetterCharacter(value)).toBeTruthy()
-    }
-  )
+  const controlCodes = [
+    0x0000, 0x0001, 0x0002, 0x0003, 0x0004, 0x0005, 0x0006, 0x0007, 0x0008,
+    0x0009, 0x000a, 0x000b, 0x000c, 0x000d, 0x000e, 0x000f, 0x0010, 0x0011,
+    0x0012, 0x0013, 0x0014, 0x0015, 0x0016, 0x0017, 0x0018, 0x001a, 0x001b,
+    0x001c, 0x001d, 0x001e, 0x001f, 0x007f, 0x0019, 0x0080, 0x0081, 0x0082,
+    0x0083, 0x0084, 0x0085, 0x0086, 0x0087, 0x0088, 0x0089, 0x0090, 0x0091,
+    0x0092, 0x0093, 0x0094, 0x0095, 0x0096, 0x008a, 0x008b, 0x008c, 0x008d,
+    0x008e, 0x008f, 0x0097, 0x0098, 0x0099, 0x009a, 0x009b, 0x009c, 0x009d,
+    0x009e, 0x009f,
+  ]
 
-  test.each(aToZUpper)(
-    'test letter character upper should be true',
-    (value) => {
-      expect(isUnicodeLetterCharacter(value)).toBeTruthy()
-    }
-  )
+  const allCharacters = [
+    ...aToZLower,
+    ...aToZUpper,
+    ...numberStrings,
+    ...numbers,
+    ...symbolCharacters,
+    ...punctuationCharacters,
+    ...separatorCharacters,
+  ]
 
-  test.each(notString)(
-    'test letter character wrong type should be false',
-    (value) => {
-      expect(isUnicodeLetterCharacter(value)).toBeFalsy()
+  test.each(allCharacters)('non control keys should return true', (value) => {
+    const event = {
+      key: value,
     }
-  )
+    expect(isPrintableUnicodeCharacterKeyPress(event)).toBeTruthy()
+  })
 
-  test.each(testNumberCases)(
-    'test letter character numbers should be false',
-    (value) => {
-      expect(isUnicodeLetterCharacter(value)).toBeFalsy()
-    }
-  )
+  test('passing string instead of event should return false', () => {
+    const charWithoutEvent = 'a'
+    expect(isPrintableUnicodeCharacterKeyPress(charWithoutEvent)).toBeFalsy()
+  })
 
-  test.each(specialCharacterCases)(
-    'test letter character special characters should be false',
-    (value) => {
-      expect(isUnicodeLetterCharacter(value)).toBeFalsy()
+  test('passing null or undefinded should return false', () => {
+    expect(isPrintableUnicodeCharacterKeyPress(null)).toBeFalsy()
+    expect(isPrintableUnicodeCharacterKeyPress(undefined)).toBeFalsy()
+  })
+
+  test.each(notString)('non string objects should return false', (value) => {
+    const event = {
+      key: value,
     }
-  )
+    expect(isPrintableUnicodeCharacterKeyPress(event)).toBeFalsy()
+  })
 
   test.each(aToZMoreThanOneChar)(
-    'test letter character with more than one characters should be false',
+    'more than one character should return false',
     (value) => {
-      expect(isUnicodeLetterCharacter(value)).toBeFalsy()
+      const event = {
+        key: value,
+      }
+      expect(isPrintableUnicodeCharacterKeyPress(event)).toBeFalsy()
     }
   )
 
-  // Tests for isNumberCharacter function
-
-  test.each(testNumberCases)(
-    'test number character should be true',
-    (value) => {
-      expect(isUnicodeNumberCharacter(value)).toBeTruthy()
+  test.each(controlCodes)('control keys should return false', (value) => {
+    const event = {
+      key: String.fromCharCode(value),
     }
-  )
-
-  test.each(testNumberCasesNumber)(
-    'test number character with number type should be true',
-    (value) => {
-      expect(isUnicodeNumberCharacter(value)).toBeTruthy()
-    }
-  )
-
-  test.each(aToZLower)(
-    'test number character with alphabetic lower character strings should be false',
-    (value) => {
-      expect(isUnicodeNumberCharacter(value)).toBeFalsy()
-    }
-  )
-
-  test.each(aToZUpper)(
-    'test number character with alphabetic upper character strings should be false',
-    (value) => {
-      expect(isUnicodeNumberCharacter(value)).toBeFalsy()
-    }
-  )
-
-  // Tests for isSpecialCharacter function
-
-  test.each(symbolCharacters)(
-    'test symbol characters should be true',
-    (value) => {
-      expect(isUnicodeSymbolCharacter(value)).toBeTruthy()
-    }
-  )
-
-  test.each(punctuationCharacters)(
-    'test punctuation characters should be true',
-    (value) => {
-      expect(isUnicodePunctuationCharacter(value)).toBeTruthy()
-    }
-  )
-
-  test.each(aToZLower)(
-    'test symbol characters with alphabetic lower characters should be false',
-    (value) => {
-      expect(isUnicodeSymbolCharacter(value)).toBeFalsy()
-    }
-  )
-
-  test.each(aToZLower)(
-    'test punctuation characters with alphabetic lower characters should be false',
-    (value) => {
-      expect(isUnicodePunctuationCharacter(value)).toBeFalsy()
-    }
-  )
-
-  test.each(aToZUpper)(
-    'test symbol characters with alphabetic upper characters should be false',
-    (value) => {
-      expect(isUnicodeSymbolCharacter(value)).toBeFalsy()
-    }
-  )
-
-  test.each(aToZUpper)(
-    'test punctuation characters with alphabetic upper characters should be false',
-    (value) => {
-      expect(isUnicodePunctuationCharacter(value)).toBeFalsy()
-    }
-  )
-
-  test.each(testNumberCases)(
-    'test symbol characters with number characters should be false',
-    (value) => {
-      expect(isUnicodeSymbolCharacter(value)).toBeFalsy()
-    }
-  )
-
-  test.each(testNumberCases)(
-    'test punctuation characters with number characters should be false',
-    (value) => {
-      expect(isUnicodePunctuationCharacter(value)).toBeFalsy()
-    }
-  )
-
-  test.each(testNumberCasesNumber)(
-    'test symbol characters with numbers should be false',
-    (value) => {
-      expect(isUnicodeSymbolCharacter(value)).toBeFalsy()
-    }
-  )
-
-  test.each(notString)(
-    'test symbol characters with different objects should be false',
-    (value) => {
-      expect(isUnicodeSymbolCharacter(value)).toBeFalsy()
-    }
-  )
-
-  test.each(testNumberCasesNumber)(
-    'test punctuation characters with numbers should be false',
-    (value) => {
-      expect(isUnicodePunctuationCharacter(value)).toBeFalsy()
-    }
-  )
-
-  test.each(notString)(
-    'test punctuation characters with different objects should be false',
-    (value) => {
-      expect(isUnicodePunctuationCharacter(value)).toBeFalsy()
-    }
-  )
+    expect(isPrintableUnicodeCharacterKeyPress(event)).toBeFalsy()
+  })
 })

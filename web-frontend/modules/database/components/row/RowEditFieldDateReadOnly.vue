efd5def536602459d733e50f3763fc0758f88e93
Sascha Jullmann <sascha@baserow.io>
2021-08-10 07:29:56 +0200

    simplified scss for component

--- web-frontend/modules/database/components/row/RowEditFieldDateReadOnly.vue
+++ web-frontend/modules/database/components/row/RowEditFieldDateReadOnly.vue
@@ -1,21 +1,10 @@
 <template>
   <div class="control__elements">
-    <div
-      class="row-edit-field-date-read-only-container"
-      :class="{
-        'row-edit-field-date-read-only--has-time': field.date_include_time,
-      }"
-    >
-      <div ref="dateDisplay" class="row-edit-field-date-read-only__date">
-        {{ getDate(field, value) }}
-      </div>
-      <div
-        v-if="field.date_include_time"
-        ref="timeDisplay"
-        class="row-edit-field-date-read-only__time"
+    <div class="field-date-read-only-timestamp">
+      {{ getDate(field, value)
+      }}<template v-if="field.date_include_time"
+        >&nbsp;{{ getTime(field, value) }}</template
       >
-        {{ getTime(field, value) }}
-      </div>
     </div>
   </div>
 </template>

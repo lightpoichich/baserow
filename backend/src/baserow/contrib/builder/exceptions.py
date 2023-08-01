29f1be7a88ddafec77892785af42766ffecd0dd7
peter_baserow <peter@baserow.io>
2023-08-01 17:17:02 +0100

    Lint

--- backend/src/baserow/contrib/builder/exceptions.py
+++ backend/src/baserow/contrib/builder/exceptions.py
@@ -1,2 +1,7 @@
 class BuilderDoesNotExist(Exception):
     """Raised when trying to get a domain that doesn't exist."""
+
+
+class InvalidIntegrationAuthorizedUser(Exception):
+    """Raised when an integration authorized user is chosen who doesn't fit our
+    criteria, usually because they're not in the workspace or marked for deletion."""
--- backend/src/baserow/contrib/integrations/local_baserow/integration_types.py
+++ backend/src/baserow/contrib/integrations/local_baserow/integration_types.py
@@ -1,7 +1,6 @@
 from typing import Any, Dict
 
 from django.contrib.auth import get_user_model
-from django.contrib.auth.models import AbstractUser
 
 from rest_framework import serializers
 

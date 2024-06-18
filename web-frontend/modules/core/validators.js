3599ee15e6c710ddca35df57ff666f3f66637abf
Armando Alvarado <mando.alvarado.jose@gmail.com>
2024-06-17 23:11:57 -0600

    Add ViewFilterTypeNumberRange component

--- web-frontend/modules/core/validators.js
+++ web-frontend/modules/core/validators.js
@@ -4,10 +4,26 @@
   
   modules/core/components/helpers/PasswordInput.vue
 */
-import { maxLength, minLength, required } from 'vuelidate/lib/validators'
+import { decimal, maxLength, minLength, required } from 'vuelidate/lib/validators'
 
 export const passwordValidation = {
   required,
   maxLength: maxLength(256),
   minLength: minLength(8),
+}
+
+/**
+ * Checks whether the provided string is in number range format.
+ * Number range format is two decimals separated by a question mark.
+ * Examples: 1?10, .1?.9, -100?-50
+ * @param {String} value The string to check
+ * @returns true if the string is in number range format
+ */
+export const numberRangeValidation = (value) => {
+  const parts = value.split("?")
+  if (parts.length !== 2) return false
+
+  const low = parts[0].trim()
+  const high = parts[1].trim()
+  return required(low) && decimal(low) && required(high) && decimal(high)
 }

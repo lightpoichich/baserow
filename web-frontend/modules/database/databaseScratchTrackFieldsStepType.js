2ccfd12ce50ce509573b6455a1621fafbde36b2d
Przemyslaw Kukulski <przemyslaw@baserow.io>
2024-08-13 16:10:33 +0200

    refactor icons

--- web-frontend/modules/database/databaseScratchTrackFieldsStepType.js
+++ web-frontend/modules/database/databaseScratchTrackFieldsStepType.js
@@ -20,27 +20,11 @@ export const fieldHandlerRegistry = {
 }
 
 export class DatabaseScratchTrackFieldsOnboardingType extends Registerable {
-  getIcons() {
-    return {
-      [LongTextFieldType.getType()]: new LongTextFieldType().iconClass,
-      [NumberFieldType.getType()]: new NumberFieldType().iconClass,
-      [DateFieldType.getType()]: new DateFieldType().iconClass,
-      [BooleanFieldType.getType()]: new BooleanFieldType().iconClass,
-      [DurationFieldType.getType()]: new DurationFieldType().iconClass,
-      [URLFieldType.getType()]: new URLFieldType().iconClass,
-      [EmailFieldType.getType()]: new EmailFieldType().iconClass,
-      [RatingFieldType.getType()]: new RatingFieldType().iconClass,
-      [SingleSelectFieldType.getType()]: new SingleSelectFieldType().iconClass,
-      [PhoneNumberFieldType.getType()]: new PhoneNumberFieldType().iconClass,
-    }
-  }
-
   static getType() {
     return 'database_scratch_track'
   }
 
   getOwnFields() {
-    const icons = this.getIcons()
     return [
       {
         name: this.$t('databaseScratchTrackFieldsStep.fields.description'),
@@ -48,7 +32,7 @@ export class DatabaseScratchTrackFieldsOnboardingType extends Registerable {
           name: this.$t('databaseScratchTrackFieldsStep.fields.description'),
           type: LongTextFieldType.getType(),
         },
-        icon: icons[LongTextFieldType.getType()],
+        icon: new LongTextFieldType().iconClass,
         rows: [
           this.$t(
             'databaseScratchTrackFieldsStep.customFields.row1.description'
@@ -69,7 +53,7 @@ export class DatabaseScratchTrackFieldsOnboardingType extends Registerable {
           number_decimal_places: 2,
           number_negative: true,
         },
-        icon: icons[NumberFieldType.getType()],
+        icon: new NumberFieldType().iconClass,
         rows: [0, -500, 131.35],
       },
       {
@@ -79,7 +63,7 @@ export class DatabaseScratchTrackFieldsOnboardingType extends Registerable {
           type: DateFieldType.getType(),
           date_format: 'ISO',
         },
-        icon: icons[DateFieldType.getType()],
+        icon: new DateFieldType().iconClass,
         rows: [
           moment().subtract(3, 'months').format('YYYY-MM-DD'),
           moment().add(1, 'days').format('YYYY-MM-DD'),
@@ -92,7 +76,7 @@ export class DatabaseScratchTrackFieldsOnboardingType extends Registerable {
           name: this.$t('databaseScratchTrackFieldsStep.fields.boolean'),
           type: BooleanFieldType.getType(),
         },
-        icon: icons[BooleanFieldType.getType()],
+        icon: new BooleanFieldType().iconClass,
         rows: [true, false, true],
       },
       {
@@ -102,7 +86,7 @@ export class DatabaseScratchTrackFieldsOnboardingType extends Registerable {
           type: DurationFieldType.getType(),
           duration_format: 'h:mm:ss',
         },
-        icon: icons[DurationFieldType.getType()],
+        icon: new DurationFieldType().iconClass,
         rows: [100, 1000, 10000],
       },
       {
@@ -111,7 +95,7 @@ export class DatabaseScratchTrackFieldsOnboardingType extends Registerable {
           name: this.$t('databaseScratchTrackFieldsStep.fields.url'),
           type: URLFieldType.getType(),
         },
-        icon: icons[URLFieldType.getType()],
+        icon: new URLFieldType().iconClass,
         rows: [
           'https://baserow.io',
           'https://example.com',
@@ -124,7 +108,7 @@ export class DatabaseScratchTrackFieldsOnboardingType extends Registerable {
           name: this.$t('databaseScratchTrackFieldsStep.fields.email'),
           type: EmailFieldType.getType(),
         },
-        icon: icons[EmailFieldType.getType()],
+        icon: new EmailFieldType().iconClass,
         rows: [
           'donnmoore@company.com',
           'gordonb@company.com',
@@ -138,7 +122,7 @@ export class DatabaseScratchTrackFieldsOnboardingType extends Registerable {
           type: RatingFieldType.getType(),
           max_value: 5,
         },
-        icon: icons[RatingFieldType.getType()],
+        icon: new RatingFieldType().iconClass,
         rows: [3, 1, 5],
       },
     ]
@@ -162,7 +146,6 @@ export class DatabaseScratchTrackProjectFieldsOnboardingType extends DatabaseScr
   }
 
   getFields() {
-    const icons = this.getIcons()
     const selectOptions = [
       {
         id: -1,
@@ -194,7 +177,7 @@ export class DatabaseScratchTrackProjectFieldsOnboardingType extends DatabaseScr
           type: SingleSelectFieldType.getType(),
           select_options: selectOptions,
         },
-        icon: icons[SingleSelectFieldType.getType()],
+        icon: new SingleSelectFieldType().iconClass,
         rows: selectOptions,
       },
       kickoffDate: {
@@ -203,7 +186,7 @@ export class DatabaseScratchTrackProjectFieldsOnboardingType extends DatabaseScr
           type: DateFieldType.getType(),
           date_format: 'ISO',
         },
-        icon: icons[DateFieldType.getType()],
+        icon: new DateFieldType().iconClass,
         rows: [
           moment().subtract(1, 'months').format('YYYY-MM-DD'),
           moment().add(1, 'weeks').format('YYYY-MM-DD'),
@@ -216,7 +199,7 @@ export class DatabaseScratchTrackProjectFieldsOnboardingType extends DatabaseScr
           type: DateFieldType.getType(),
           date_format: 'ISO',
         },
-        icon: icons[DateFieldType.getType()],
+        icon: new DateFieldType().iconClass,
         rows: [
           moment().subtract(1, 'months').format('YYYY-MM-DD'),
           moment().add(1, 'days').format('YYYY-MM-DD'),
@@ -230,7 +213,7 @@ export class DatabaseScratchTrackProjectFieldsOnboardingType extends DatabaseScr
           number_decimal_places: 0,
           number_negative: false,
         },
-        icon: icons[NumberFieldType.getType()],
+        icon: new NumberFieldType().iconClass,
         rows: [500, 1000, 3000],
       },
       completed: {
@@ -238,7 +221,7 @@ export class DatabaseScratchTrackProjectFieldsOnboardingType extends DatabaseScr
           name: this.$t('databaseScratchTrackFieldsStep.fields.completed'),
           type: BooleanFieldType.getType(),
         },
-        icon: icons[BooleanFieldType.getType()],
+        icon: new BooleanFieldType().iconClass,
         rows: [true, false, false],
       },
       notes: {
@@ -246,7 +229,7 @@ export class DatabaseScratchTrackProjectFieldsOnboardingType extends DatabaseScr
           name: this.$t('databaseScratchTrackFieldsStep.fields.notes'),
           type: LongTextFieldType.getType(),
         },
-        icon: icons[LongTextFieldType.getType()],
+        icon: new LongTextFieldType().iconClass,
         rows: [
           this.$t('databaseScratchTrackFieldsStep.projects.row1.notes'),
           this.$t('databaseScratchTrackFieldsStep.projects.row2.notes'),
@@ -263,7 +246,6 @@ export class DatabaseScratchTrackTeamFieldsOnboardingType extends DatabaseScratc
   }
 
   getFields() {
-    const icons = this.getIcons()
     const selectOptions = [
       {
         id: 1,
@@ -288,7 +270,7 @@ export class DatabaseScratchTrackTeamFieldsOnboardingType extends DatabaseScratc
           type: SingleSelectFieldType.getType(),
           select_options: selectOptions,
         },
-        icon: icons[SingleSelectFieldType.getType()],
+        icon: new SingleSelectFieldType().iconClass,
         rows: selectOptions,
       },
       phone: {
@@ -296,7 +278,7 @@ export class DatabaseScratchTrackTeamFieldsOnboardingType extends DatabaseScratc
           name: this.$t('databaseScratchTrackFieldsStep.fields.phone'),
           type: PhoneNumberFieldType.getType(),
         },
-        icon: icons[SingleSelectFieldType.getType()],
+        icon: new PhoneNumberFieldType().iconClass,
         rows: ['(508) 398-0845', '(803) 996-6704', '(269) 445-2068'],
       },
       email: {
@@ -304,7 +286,7 @@ export class DatabaseScratchTrackTeamFieldsOnboardingType extends DatabaseScratc
           name: this.$t('databaseScratchTrackFieldsStep.fields.email'),
           type: EmailFieldType.getType(),
         },
-        icon: icons[EmailFieldType.getType()],
+        icon: new EmailFieldType().iconClass,
         rows: [
           'donnmoore@company.com',
           'gordonb@company.com',
@@ -316,7 +298,7 @@ export class DatabaseScratchTrackTeamFieldsOnboardingType extends DatabaseScratc
           name: this.$t('databaseScratchTrackFieldsStep.fields.active'),
           type: BooleanFieldType.getType(),
         },
-        icon: icons[BooleanFieldType.getType()],
+        icon: new BooleanFieldType().iconClass,
         rows: [true, false, true],
       },
     }
@@ -329,7 +311,6 @@ export class DatabaseScratchTrackTaskFieldsOnboardingType extends DatabaseScratc
   }
 
   getFields() {
-    const icons = this.getIcons()
     return {
       estimatedDays: {
         props: {
@@ -338,7 +319,7 @@ export class DatabaseScratchTrackTaskFieldsOnboardingType extends DatabaseScratc
           number_decimal_places: 0,
           number_negative: false,
         },
-        icon: icons[NumberFieldType.getType()],
+        icon: new NumberFieldType().iconClass,
         rows: [2, 7, 13],
       },
       completed: {
@@ -346,7 +327,7 @@ export class DatabaseScratchTrackTaskFieldsOnboardingType extends DatabaseScratc
           name: this.$t('databaseScratchTrackFieldsStep.fields.completed'),
           type: BooleanFieldType.getType(),
         },
-        icon: icons[BooleanFieldType.getType()],
+        icon: new BooleanFieldType().iconClass,
         rows: [true, false, false],
       },
       description: {
@@ -354,7 +335,7 @@ export class DatabaseScratchTrackTaskFieldsOnboardingType extends DatabaseScratc
           name: this.$t('databaseScratchTrackFieldsStep.fields.description'),
           type: LongTextFieldType.getType(),
         },
-        icon: icons[LongTextFieldType.getType()],
+        icon: new LongTextFieldType().iconClass,
         rows: [
           this.$t('databaseScratchTrackFieldsStep.tasks.row1.description'),
           this.$t('databaseScratchTrackFieldsStep.tasks.row2.description'),
@@ -371,14 +352,13 @@ export class DatabaseScratchTrackCampaignFieldsOnboardingType extends DatabaseSc
   }
 
   getFields() {
-    const icons = this.getIcons()
     return {
       description: {
         props: {
           name: this.$t('databaseScratchTrackFieldsStep.fields.description'),
           type: LongTextFieldType.getType(),
         },
-        icon: icons[LongTextFieldType.getType()],
+        icon: new LongTextFieldType().iconClass,
         rows: [
           this.$t('databaseScratchTrackFieldsStep.campaigns.row1.description'),
           this.$t('databaseScratchTrackFieldsStep.campaigns.row2.description'),
@@ -391,7 +371,7 @@ export class DatabaseScratchTrackCampaignFieldsOnboardingType extends DatabaseSc
           type: DateFieldType.getType(),
           date_format: 'ISO',
         },
-        icon: icons[DateFieldType.getType()],
+        icon: new DateFieldType().iconClass,
         rows: [
           moment().subtract(1, 'months').format('YYYY-MM-DD'),
           moment().add(1, 'weeks').format('YYYY-MM-DD'),
@@ -404,7 +384,7 @@ export class DatabaseScratchTrackCampaignFieldsOnboardingType extends DatabaseSc
           type: DateFieldType.getType(),
           date_format: 'ISO',
         },
-        icon: icons[DateFieldType.getType()],
+        icon: new DateFieldType().iconClass,
         rows: [
           moment().subtract(1, 'months').format('YYYY-MM-DD'),
           moment().add(1, 'days').format('YYYY-MM-DD'),
@@ -418,7 +398,7 @@ export class DatabaseScratchTrackCampaignFieldsOnboardingType extends DatabaseSc
           number_decimal_places: 2,
           number_negative: false,
         },
-        icon: icons[NumberFieldType.getType()],
+        icon: new NumberFieldType().iconClass,
         rows: [12000, 30000, 2000],
       },
     }
@@ -431,7 +411,6 @@ export class DatabaseScratchTrackCustomFieldsOnboardingType extends DatabaseScra
   }
 
   getFields() {
-    const icons = this.getIcons()
     return {
       date: {
         props: {
@@ -439,7 +418,7 @@ export class DatabaseScratchTrackCustomFieldsOnboardingType extends DatabaseScra
           type: DateFieldType.getType(),
           date_format: 'ISO',
         },
-        icon: icons[DateFieldType.getType()],
+        icon: new DateFieldType().iconClass,
         rows: [
           moment().subtract(1, 'months').format('YYYY-MM-DD'),
           moment().add(1, 'weeks').format('YYYY-MM-DD'),
@@ -453,7 +432,7 @@ export class DatabaseScratchTrackCustomFieldsOnboardingType extends DatabaseScra
           number_decimal_places: 0,
           number_negative: true,
         },
-        icon: icons[NumberFieldType.getType()],
+        icon: new NumberFieldType().iconClass,
         rows: [500, -1000, 3000],
       },
       completed: {
@@ -461,7 +440,7 @@ export class DatabaseScratchTrackCustomFieldsOnboardingType extends DatabaseScra
           name: this.$t('databaseScratchTrackFieldsStep.fields.completed'),
           type: BooleanFieldType.getType(),
         },
-        icon: icons[BooleanFieldType.getType()],
+        icon: new BooleanFieldType().iconClass,
         rows: [true, false, false],
       },
       description: {
@@ -469,7 +448,7 @@ export class DatabaseScratchTrackCustomFieldsOnboardingType extends DatabaseScra
           name: this.$t('databaseScratchTrackFieldsStep.fields.description'),
           type: LongTextFieldType.getType(),
         },
-        icon: icons[LongTextFieldType.getType()],
+        icon: new LongTextFieldType().iconClass,
         rows: [
           this.$t(
             'databaseScratchTrackFieldsStep.customFields.row1.description'

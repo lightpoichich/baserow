72953611baf8397bf150288216c3a757e8b20c55
Bram Wiepjes <bramw@protonmail.com>
2024-09-10 17:32:32 +0200

    added feature flag

--- backend/src/baserow/contrib/database/api/data_sync/urls.py
+++ backend/src/baserow/contrib/database/api/data_sync/urls.py
@@ -1,16 +1,20 @@
 from django.urls import re_path
+from django.conf import settings
 
 from .views import DataSyncsView, SyncDataSyncTableView
 
-app_name = "baserow.contrib.database.api.data_sync"
 
-urlpatterns = [
-    re_path(
-        r"database/(?P<database_id>[0-9]+)/$", DataSyncsView.as_view(), name="list"
-    ),
-    re_path(
-        r"(?P<data_sync_id>[0-9]+)/sync/async/$",
-        SyncDataSyncTableView.as_view(),
-        name="sync_table",
-    ),
-]
+app_name = "baserow.contrib.database.api.data_sync"
+urlpatterns = []
+
+if "data_sync" in settings.FEATURE_FLAGS:
+    urlpatterns = [
+        re_path(
+            r"database/(?P<database_id>[0-9]+)/$", DataSyncsView.as_view(), name="list"
+        ),
+        re_path(
+            r"(?P<data_sync_id>[0-9]+)/sync/async/$",
+            SyncDataSyncTableView.as_view(),
+            name="sync_table",
+        ),
+    ]

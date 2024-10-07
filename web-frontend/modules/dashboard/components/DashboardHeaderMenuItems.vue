dbf879d52d0fbec85ea97dd3ddd27de469c242b9
Petr Stribny <petr@stribny.name>
2024-10-07 14:50:14 +0200

    Introduce edit mode for dashboards

--- web-frontend/modules/dashboard/components/DashboardHeaderMenuItems.vue
+++ web-frontend/modules/dashboard/components/DashboardHeaderMenuItems.vue
@@ -1,8 +1,19 @@
 <template>
-  <div></div>
+  <div class="header__filter">
+    <li class="header__filter-item">
+      <a class="header__filter-link" @click="toggleEditMode">
+        <i class="header__filter-icon iconoir-edit"></i>
+        <span class="header__filter-name">{{
+          $t('dashboardHeaderMenuItems.editMode')
+        }}</span>
+      </a>
+    </li>
+  </div>
 </template>
 
 <script>
+import { mapActions } from 'vuex'
+
 export default {
   name: 'DashboardHeaderMenuItems',
   props: {
@@ -10,6 +21,11 @@ export default {
       type: Object,
       required: true,
     },
+  },
+  methods: {
+    ...mapActions({
+      toggleEditMode: 'dashboardApplication/toggleEditMode',
+    }),
   },
 }
 </script>

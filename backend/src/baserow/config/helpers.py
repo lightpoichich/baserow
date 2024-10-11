333ab0b213f2fa7d0bac08c362068dec4cf1df50
Cezary Statkiewicz <cezio@thelirium.net>
2024-10-11 18:31:49 +0200

    lint fix

--- backend/src/baserow/config/helpers.py
+++ backend/src/baserow/config/helpers.py
@@ -1,5 +1,7 @@
 import asyncio
+
 from loguru import logger
+
 
 class dummy_context:
     async def __aenter__(self):
@@ -17,7 +19,7 @@ class ConcurrencyLimiterASGI:
 
     def __init__(self, app, max_concurrency: int | None = None):
         self.app = app
-        logger.info(f'Setting ASGI app concurrency to {max_concurrency}')
+        logger.info(f"Setting ASGI app concurrency to {max_concurrency}")
         self.semaphore = (
             asyncio.Semaphore(max_concurrency)
             if (isinstance(max_concurrency, int) and max_concurrency > 0)
